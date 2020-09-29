848d96fba43e2d1f13077938bbf3b71b9f7f60a0
sakatosi0728 <sakatosi0728@gmail.com>
2020-09-29 23:06:11 +0900

    日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rooms
   has_many :messages
+
+  with_options presence: true do 
+    validates :nickname
+    validates :email, format: { with: /\A\S+@\S+\.\S+\z/}, uniqueness: true
+  end
 end
